195bfdab1c1f27df185734bd236e64a8a5fd401f
Jaime Alvarez <L22211519@tijuana.tecnm.mx>
2026-02-14 14:22:30 -0800

    Se modifica el metodo get_response() para mostrar un html dinamico que cambia, dependiendo la ruta y el autor.
    Resultado:
    
    PS D:\Programacion Web\Actividad_Unidad_1> curl http://localhost:8000/proyecto/actividad_1?autor=Jaime
    
    StatusCode        : 200
    StatusDescription : OK
    Content           : <h1>Proyecto: actividad_1 Autor: Jaime</h1>
                                        <h1> Hola Web </h1>
                                        <p> URL Parse Result : ParseResult(scheme='', netloc='', path='/proyecto/actividad_1', params='', query=...
    RawContent        : HTTP/1.0 200 OK
                        Content-Type: text/html
                        Date: Sat, 14 Feb 2026 22:17:55 GMT
                        Server: BaseHTTP/0.6 Python/3.13.11
    
                        <h1>Proyecto: actividad_1 Autor: Jaime</h1>
                                        <h1> Hola Web </h1>
                          ...

--- web.py
+++ web.py
@@ -15,6 +15,11 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         return dict(parse_qsl(self.url().query))
 
     def do_GET(self):
+        # HTML dinamico que muestra la ruta y el actor
+        path = self.url().path
+        parametros = self.query_data()
+
+
         # Impresion de los datos requeridos para el request
         print("# Request #")
         print(f"Host: {self.headers.get('Host')}")
@@ -22,21 +27,26 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         print(f"Ruta: {self.path}")
         print(" ")
 
-
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
         self.end_headers()
         self.wfile.write(self.get_response().encode("utf-8"))
 
     def get_response(self):
-        return f"""
-    <h1> Hola Web </h1>
-    <p> URL Parse Result : {self.url()}         </p>
-    <p> Path Original: {self.path}         </p>
-    <p> Headers: {self.headers}      </p>
-    <p> Query: {self.query_data()}   </p>
-"""
+        path = self.url().path
+        parametros = self.query_data()
 
+        # Obtener el nombre del proyecto
+        if path.startswith('/proyecto/'):
+            proyecto = path.split('/')[-1]
+            autor = parametros.get('autor', 'Anonimo')
+            return f"""<h1>Proyecto: {proyecto} Autor: {autor}</h1>
+                <h1> Hola Web </h1>
+                <p> URL Parse Result : {self.url()} </p>
+                <p> Path Original: {self.path} </p>
+                <p> Headers: {self.headers} </p>
+                <p> Query: {self.query_data()} </p>
+            """
 
 if __name__ == "__main__":
     print("Starting server")
